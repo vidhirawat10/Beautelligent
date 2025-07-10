e02e9992fe438631ed9323cc4723062b4e20c9d6
vidhirawat10 <vidhirawat54@gmail.com>
2025-07-10 19:51:01 +0530

    Frontend Done(Majorly)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,26 @@
-// src/App.js (or your main routing file)
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import CaptureImage from './CaptureImage'; // Assuming you have this
-import UploadImage from './UploadImage';   // Assuming you have this
-import AnalysisResultPage from './AnalysisResultPage'; // Assuming you have this
-import RecommendationsPage from './RecommendationsPage'; // Import the new page
+import BeautelligentHomePage from "./Homepage";
+import LoginPage from './LoginPage';
+import AnalysisPage from './AnalysisPage';
+import UploadImage from './UploadImage';
+import CaptureImage from './CaptureImage';
+import AnalysisResultPage from './AnalysisResultPage';
+import RecommendationsPage from './RecommendationsPage'; // Import the new AnalysisResultPage
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<CaptureImage />} /> {/* Or your landing page */}
-        <Route path="/capture-image" element={<CaptureImage />} />
+        <Route path="/" element={<BeautelligentHomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/analysis" element={<AnalysisPage />} />
         <Route path="/upload-image" element={<UploadImage />} />
-        <Route path="/analysis-result" element={<AnalysisResultPage />} />
-        <Route path="/recommendations" element={<RecommendationsPage />} /> {/* New Route */}
+        <Route path="/capture-image" element={<CaptureImage />} />
+        <Route path="/analysis-result" element={<AnalysisResultPage />} /> 
+        <Route path="/recommendations" element={<RecommendationsPage />} />{/* New route */}
+        {/* You can add more routes here for other pages like /technology, /products, etc. */}
       </Routes>
     </Router>
   );
